32eb2b7af7a9b59990b828a6bcd6de512b2704c2
BlackyHat <goodwin009@gmail.com>
2024-05-29 17:12:10 +0300

    feat: adding logo component, styles, readme description, and placed in the header and footer

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,7 +1,12 @@
+import { Logo } from '@/components/ui';
+
 export const Header: React.FC = () => {
   return (
     <header className="bg-emerald-400">
-      <div className="container">Header</div>
+      <div className="container">
+        <Logo variant="brown" />
+        Header
+      </div>
     </header>
   );
 };
