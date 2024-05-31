5a5320fd080553ba3693b1dd8fe2979e0dae7240
Denys Borysiuk <denisborisiuk@gmail.com>
2024-05-31 18:25:14 +0300

    add benefit card

--- src/components/base/index.tsx
+++ src/components/base/index.tsx
@@ -1,3 +1,4 @@
 export * from '../base/GoogleMaps';
 export * from './ReviewForm';
 export * from './BlogArticle';
+export * from './BenefitsList';
---
+++ src/components/ui/BenefitCard/types.ts
@@ -0,0 +1,5 @@
+export type BenefitCardProps = {
+  title: string;
+  text: string;
+  idx: number;
+};
