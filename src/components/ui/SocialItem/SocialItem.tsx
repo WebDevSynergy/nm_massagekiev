a88d46ee59436dad553d062e4631c3acd4d8f571
VitaBabenko <babenkovita1988@gmail.com>
2024-05-31 19:22:49 +0300

    fix link

--- src/components/ui/SocialItem/SocialItem.tsx
+++ src/components/ui/SocialItem/SocialItem.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 import FacebookIcon from '~/icons/facebook.svg';
@@ -14,17 +15,12 @@ export const SocialItem: React.FC<SocialItemProps> = ({
 }) => {
   return (
     <li className="flex size-10 cursor-pointer items-center justify-center rounded-[100%] bg-greenLight text-green transition hover:bg-green hover:text-white focus:bg-green focus:text-white">
-      <a
-        href={href}
-        aria-label={ariaLabel}
-        target="_blank"
-        rel="noopener noreferrer nofollow"
-      >
+      <Link href={href} aria-label={ariaLabel} target="_blank">
         {name === 'instagram' && <InstagramIcon width={22} height={22} />}
         {name === 'viber' && <ViberIcon width={24} height={24} />}
         {name === 'telegram' && <TelegramIcon width={24} height={24} />}
         {name === 'facebook' && <FacebookIcon width={24} height={24} />}
-      </a>
+      </Link>
     </li>
   );
 };
