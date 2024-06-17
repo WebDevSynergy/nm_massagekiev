a68a0cc091f09dcee6b1cd1ae23ba6d3dc8620fa
BlackyHat <goodwin009@gmail.com>
2024-06-17 13:46:15 +0300

    ref: add id section contact us

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -1,12 +1,14 @@
 import { ButtonLink, GoogleRating, SectionTitle } from '@/components/ui';
 
+import contactData from '@/data/contactUs-form.json';
+
 export const HeroSection: React.FC = () => {
   return (
     <section className="section">
       <div className="container">
         <SectionTitle>HeroSection</SectionTitle>
         <div className="flex flex-wrap items-center justify-between gap-2">
-          <ButtonLink type="button" styleType="primary">
+          <ButtonLink tag="link" href={contactData.id} styleType="primary">
             Записатися
           </ButtonLink>
           <ButtonLink tag="link" href="#services" styleType="secondary">
