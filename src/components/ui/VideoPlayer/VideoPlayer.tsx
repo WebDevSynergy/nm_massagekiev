a244455a1ab701bd1286bd6d65b3d9f8e778990c
A-Novychenko <A.Novychenko@outlook.com>
2024-06-20 23:33:14 +0300

    fix video

--- src/components/ui/VideoPlayer/VideoPlayer.tsx
+++ src/components/ui/VideoPlayer/VideoPlayer.tsx
@@ -8,8 +8,35 @@ import { VideoPlayerProps } from './types';
 
 export const VideoPlayer: React.FC<VideoPlayerProps> = ({ url }) => {
   return (
-    <div className="h-[328px] w-full overflow-hidden rounded-[24px] md:size-[688px] xl:size-[588px] 2xl:size-[686px]">
-      <ReactPlayer loop muted playing url={url} width="100%" height="100%" />
+    <div
+      className="relative size-[448px] overflow-hidden rounded-[24px] md:size-[688px] md:rounded-[32px] 
+      xl:size-[588px] 2xl:size-[686px] 2xl:rounded-[40px] sm480:max-h-[448px] sm480:max-w-full"
+    >
+      <ReactPlayer
+        loop
+        muted
+        playing
+        url={url}
+        controls={false}
+        config={{
+          youtube: {
+            playerVars: {
+              showinfo: 0,
+              controls: 0,
+              modestbranding: 1,
+              fs: 0,
+              iv_load_policy: 3,
+              disablekb: 1,
+              rel: 0,
+              autoplay: 1,
+              mute: 1,
+            },
+          },
+        }}
+        width="100%"
+        height="100%"
+        className="pointer-events-none absolute left-0 top-0 -z-10 "
+      />
     </div>
   );
 };
